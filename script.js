39c55dedae86cde0726d4c74b93ecd9fbb58b2f0
Kavya <kavyamalthi6526@gmail.com>
2026-01-21 18:39:52 +0530

    Updated AppScript URL

--- script.js
+++ script.js
@@ -182,8 +182,8 @@ function setupFeedbackForm() {
     };
 
     const SCRIPT_URL =
-            "https://script.google.com/macros/s/AKfycbzCC-xdh2-hdeGgD34-6EvD_LmZd5edsHchlufo7YFKf7V6I6YEct17Vsryitp5aQ8Erw/exec";
-    fetch(SCRIPT_URL, {
+        "https://script.google.com/macros/s/AKfycbz8NnoBdaEPR4g3o-XaeQMnlii6-DUbdNZag36I1adXKLA9CwAprlvKZseSe9n6HMjXzw/exec"
+      fetch(SCRIPT_URL, {
       method: "POST",
       mode: "no-cors",
       headers: { "Content-Type": "application/json" },
